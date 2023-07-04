578a799e2b924ae399d011af32daf4762a62fa11
Urias Villagra <emavillagra00@gmail.com>
2023-07-03 23:14:47 -0300

    PreEntrega1Villagra

---
+++ src/components/ItemContainer.jsx
@@ -0,0 +1,7 @@
+export const ItemContainer = () => {
+    return (
+        <div>
+            <h1>ItemContainer</h1>
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+export const Navbar = () => {
+
+
+
+    return (
+    <>
+        <header>
+            <nav>
+                <ul class="titulo"><li><h1>E-commerce (nombre a definir)</h1></li></ul>
+                <ul class="menu_navegacion">
+                    <li><button>Inicio</button></li>
+                    <li><button>Productos mas Vendidos</button></li>
+                    <li><button>Contacto</button></li>
+                </ul>
+            </nav>
+        </header>
+
+    </>
+    )
+}
